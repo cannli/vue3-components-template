4a77aaff2ca8b7ee6ba1e79e798d8d4fd830ca4a
v_cannli <v_cannli@tencent.com>
2021-11-01 15:33:17 +0800

    1

---
+++ main-components/src/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :class="classObj" class="app-wrapper"></div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        computed: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
